1a2d436718ffafbb6bfed52cf4f0113e8a588544
Estefanía Marmolejo <esmarmolejosa@ittepic.edu.mx>
2023-03-19 21:29:39 -0700

    DSW

---
+++ src/api/v1/routes/carreras.routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import * as carrerasController from '../controllers/carreras.controller';
+const router = Router();
+//ficRouter.get('/list', ProdServController.getProdServList);
+router.get('/', carrerasController.getCarrerasList);
+//router.get('/item/:ficIdProdServ', prodServController.getProdServItem);
+router.get('/:id', carrerasController.getCarrerasItem);
+router.post('/', carrerasController.posCarrerasItem);
+router.put('/:id', carrerasController.putCarrerasItem);
+export default router;
